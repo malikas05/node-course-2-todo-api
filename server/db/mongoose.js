9da52806c53c9342c91504fc719b69b94b237f7e
malik.iavari <malik.iavari@centro.net>
2018-10-26 23:13:20 -0400

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://malikas:malikas241@ds143573.mlab.com:43573/node-todo-app');
 
 module.exports = {
     mongoose: mongoose
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
     }, (e) => res.status(400).send(e));
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
