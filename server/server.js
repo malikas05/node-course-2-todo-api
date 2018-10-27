1117f43d3d43e6969118bc5d17c4c3b5a1c41e82
malik.iavari <malik.iavari@centro.net>
2018-10-26 22:08:41 -0400

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -27,7 +29,22 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo){
+            res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }, (e) => res.status(400).send(e));
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
